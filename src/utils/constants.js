5ef64c352c536962da1d05598d98cbf78fe60239
Sanju-Barsiwal <sanjeev.kumar@iiitg.ac.in>
2025-12-22 22:43:52 +0530

    deleted console

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-console.log('🔍 VITE_API_URL:', import.meta.env.VITE_API_URL);
-console.log('🔍 BASE_URL:', import.meta.env.VITE_API_URL || 'http://localhost:7777');
+
 
 export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:7777';
